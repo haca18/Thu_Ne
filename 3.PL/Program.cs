a10101dfc4b41fb7bd5c13d21308fa3610a5af40
tonnbph26190 <115675029+tonnbph26190@users.noreply.github.com>
2022-11-15 19:46:25 +0700

    Update Program.cs

--- 3.PL/Program.cs
+++ 3.PL/Program.cs
@@ -17,7 +17,7 @@ namespace _3.PL
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());ton120
+            //Application.Run(new Form1());ton12112003
         }
     }
 }
