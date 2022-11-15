233369c350678682fb68721c8955fd720470c27b
Tonba03 <tonnbph26190@fpt.edu.vn>
2022-11-15 23:17:33 +0700

    sua cai j do

--- 3.PL/Program.cs
+++ 3.PL/Program.cs
@@ -17,7 +17,7 @@ namespace _3.PL
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());ton1211200342323
+            //Application.Run(new Form1());ton1212222
         }
     }
 }
