37359c001c7303373aa43e96a07955bfd083874c
Tonba03 <tonnbph26190@fpt.edu.vn>
2022-11-17 12:37:15 +0700

    oke

--- 3.PL/ViewModel/SanPhamKhuyenMaiView.cs
+++ 3.PL/ViewModel/SanPhamKhuyenMaiView.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using _1.DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _3.PL.ViewModel
 {
-    internal class SanPhamKhuyenMaiView
+    public class SanPhamKhuyenMaiView
     {
+        public Guid? Id { get; set; }
+        [StringLength(50)]
+        public int? TrangThai { get; set; }
+        public Guid? IdKhuyenMai { get; set; }
+        public Guid? IdSanPham { get; set; }
+        public virtual KhuyenMai? KhuyenMai { get; set; }
+        public virtual SanPham? SanPham { get; set; }
+        //chans
     }
 }
